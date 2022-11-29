29b45fc5032951c12aa4130ede076e205016ff93
Brandon Wright <soulfuloops@gmail.com>
2022-11-28 20:49:43 -0500

    Create ytdlmp3.py
    YTDLP script added

---
+++ ytdlmp3.py
@@ -0,0 +1,7 @@
+##convert my favorite youtube channels to m3.. because im a data horder ;-\
+
+## In this, I will be using YTDLP
+
+## convert to mp3 + embed thumbnail
+
+mp3 = "yt-dlp -o "%(title)s.%(ext)s" -x --audio-format mp3 --add-metadata --embed-thumbnail"
